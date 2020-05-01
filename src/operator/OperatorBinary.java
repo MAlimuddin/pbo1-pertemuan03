4e8ec9b30a52d0e47ed6a384582c18964a4284c9
MAlimuddin <muddinali23@gmail.com>
2020-05-01 09:20:50 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -5,4 +5,14 @@ public class OperatorBinary {
     String nama = "Muhammad Alimuddin";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+        
+    }
 }
